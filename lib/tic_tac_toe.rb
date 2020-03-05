171c306497dd5252675814dc12b211712a6c2316
Don Namgyal <dhondup.namgyal@gmail.com>
2020-03-05 22:03:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,7 +6,6 @@ def play(board)
   if !over?(board)
     turn(board)
     draw?(board)
-    over?(board)
   end
   if won?(board)
     champ = winner(board)
@@ -18,7 +17,17 @@ end
 
 
 
-
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
 
 
 
@@ -34,7 +43,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
+def move(board, index, current_player)
   board[index] = current_player
 end
 
@@ -46,17 +55,7 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
+
 
 def turn_count(board)
   counter = 0
