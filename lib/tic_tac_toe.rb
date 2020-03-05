26378d79d0ab3e8f077ff019af0ca3bd5982f4cb
Don Namgyal <dhondup.namgyal@gmail.com>
2020-03-05 21:46:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,3 +37,21 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  counter = 0
+  board.each do |val|
+    if val == 'X' || val == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  x = turn_count(board)
+  if x % 2 == 0
+    return 'X'
+  else return 'O'
+  end
+end
