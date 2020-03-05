321c09478bc40f3d47935e489e255fa54d5b15cb
Don Namgyal <dhondup.namgyal@gmail.com>
2020-03-05 21:44:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,7 @@
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+]
+
 def display_board(moves)
   puts " #{moves[0]} | #{moves[1]} | #{moves[2]} "
   puts "-----------"
