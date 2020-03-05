8d98c1aee47b0f857531047620a7dd63dc3799cb
Don Namgyal <dhondup.namgyal@gmail.com>
2020-03-05 21:49:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,6 +2,21 @@ WIN_COMBINATIONS = [
   [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
 ]
 
+def play(board)
+  if !over?(board)
+    turn(board)
+  end
+
+  if won?(board)
+    champ = winner(board)
+    puts "The winner is #{champ}."
+
+
+
+
+
+
+
 def display_board(moves)
   puts " #{moves[0]} | #{moves[1]} | #{moves[2]} "
   puts "-----------"
